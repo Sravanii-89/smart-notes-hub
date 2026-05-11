f7b72c0deb03eed0600babf5b8eda90378a5fa4f
sravani <mullusravani0408@gmail.com>
2026-05-11 23:07:07 +0530

    restore upload page

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -2,20 +2,65 @@
 
 export const dynamic = "force-dynamic";
 
+import {
+  useState,
+} from "react";
+
+import { supabase } from "@/lib/supabase";
+
+import {
+  motion,
+} from "framer-motion";
+
+const branches = [
+  "firstYear",
+  "CSE",
+  "IT",
+  "ECE",
+  "EEE",
+  "MECH",
+  "CIVIL",
+];
+
+const years = [
+  "firstYear",
+  "secondYear",
+  "thirdYear",
+  "fourthYear",
+];
+
 export default function UploadPage() {
-  return (
-    <div
-      className="
-      min-h-screen
-      flex
-      items-center
-      justify-center
-      bg-black
-      text-white
-      text-4xl
-      font-bold"
-    >
-      Upload Page
-    </div>
-  );
+
+  const [title, setTitle] =
+    useState("");
+
+  const [subject, setSubject] =
+    useState("");
+
+  const [branch, setBranch] =
+    useState("");
+
+  const [year, setYear] =
+    useState("");
+
+  const [file, setFile] =
+    useState<File | null>(null);
+
+  const [loading, setLoading] =
+    useState(false);
+
+  async function handleUpload() {
+
+    if (
+      !title ||
+      !subject ||
+      !branch ||
+      !year ||
+      !file
+    ) {
+
+      alert(
+        "Please fill all fields"
+      );
+
 }
